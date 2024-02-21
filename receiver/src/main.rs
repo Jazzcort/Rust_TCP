598e6d193f0fd360500c40a33ad8f66a38b040b0
jazzcort <jason101011113@gmail.com>
2024-02-20 22:54:15 -0500

    testing

--- receiver/src/main.rs
+++ receiver/src/main.rs
@@ -1,15 +1,26 @@
-use std::net::UdpSocket;
 use std::io::{self, Write};
+use std::net::UdpSocket;
 
 fn main() {
     let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
+    socket.set_nonblocking(true).unwrap();
     eprintln!("Bound to port {}", socket.local_addr().unwrap().port());
 
+    
+
     let mut buf: [u8; 1500] = [0; 1500];
-    socket.recv(&mut buf).unwrap();   
+    loop {
+        match socket.recv(&mut buf) {
+            Ok(_) => {
+                print!("{}", String::from_utf8_lossy(&buf));
+                buf.fill(0);
+            }
+            Err(_) => {}
+        }
+    }
 
-    let mut output = io::stdout();
-    output.write(&buf).unwrap();
+    // let mut output = io::stdout();
+    // output.write(&buf).unwrap();
 
-    println!("{}", String::from_utf8_lossy(&buf));
+    // println!("{}", String::from_utf8_lossy(&buf));
 }
--- sender/src/main.rs
+++ sender/src/main.rs
@@ -1,6 +1,7 @@
 use std::io::{self, BufRead};
 use clap::Parser;
 use std::net::UdpSocket;
+use std::thread::{JoinHandle, spawn};
 
 #[derive(Parser, Debug)]
 #[command(author, about, long_about = None)]
@@ -12,7 +13,8 @@ struct Cli {
 fn main() -> Result<(), String> {
     let cli = Cli::parse();
 
-    let scoket = UdpSocket::bind("127.0.0.1:0").unwrap();
+    let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
+    
 
 
     let mut buffer = String::new();
@@ -20,16 +22,45 @@ fn main() -> Result<(), String> {
     let mut handle = stdin.lock();
 
     let data_len = handle.read_line(&mut buffer).map_err(|e| format!("{e} -> failed to read"))?;
-    // println!("Read {} bytes", data_len);
+    println!("Read {} bytes", data_len);
     // println!("{} from rust", buffer);
 
-    let (first, second) = buffer.split_at(1480);
-    eprintln!("{}", first);
+    let texts: Vec<String> = buffer.as_bytes().chunks(1500).map(| ch| String::from_utf8_lossy(ch).to_string()).collect();
+   
+    let mut handles: Vec<JoinHandle<()>> = vec!();
+    let host = cli.recv_host.clone();
+    let port = cli.recv_port.clone();
+    
+    
+    let mut i = 0;
 
-    match scoket.send_to(first.as_bytes(), format!("{}:{}", cli.recv_host, cli.recv_port)) {
-        Ok(_) => {eprintln!("Success")}
-        Err(_) => {eprintln!("Fail")}
+    for mut t in texts.into_iter() {
+        let socket_copy = socket.try_clone().unwrap();
+        let a = host.clone();
+        let b = port.clone();
+        // t.push_str(format!("***{}***", i).as_str());
+        // dbg!(&t);
+        i += 1;
+
+        // let (host, port) = (&cli.recv_host.clone(), cli.recv_port.clone());
+        let handle = spawn(move || {
+            socket_copy.send_to(t.as_bytes() , format!("{}:{}", a, b)).unwrap();
+        });
+
+        handles.push(handle);
     }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+
+    // eprintln!("{}", first);
+
+    // match socket.send_to(first.as_bytes(), format!("{}:{}", cli.recv_host, cli.recv_port)) {
+    //     Ok(_) => {eprintln!("Success")}
+    //     Err(_) => {eprintln!("Fail")}
+    // }
     // scoket.send_to(second.as_bytes(), format!("{}:{}", cli.recv_host, cli.recv_port)).unwrap();
     Ok(())
 }
