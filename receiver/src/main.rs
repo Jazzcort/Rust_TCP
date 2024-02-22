441322012924f69a3e5bff07c9aff553d8505369
Jen Ting Huang <jentinghuang@Jens-MacBook-Pro.local>
2024-02-21 23:34:39 -0500

    implemented tcp header

--- receiver/src/main.rs
+++ receiver/src/main.rs
@@ -1,15 +1,34 @@
 use std::net::UdpSocket;
 use std::io::{self, Write};
 
+mod tcp_header;
+
+use tcp_header::TcpHeader;
+
 fn main() {
-    let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
-    eprintln!("Bound to port {}", socket.local_addr().unwrap().port());
 
-    let mut buf: [u8; 1500] = [0; 1500];
-    socket.recv(&mut buf).unwrap();   
+    // Example TCP header bytes
+    let header_bytes: [u8; 16] = [
+        0x00, 0x50, // Source port (80)
+        0x01, 0xBB, // Destination port (443)
+        0x00, 0x00, 0x00, 0x01, // Sequence number
+        0x00, 0x00, 0x00, 0x00, // Acknowledgment number
+        0x50, // Header length (5) and Reserved (0), here for simplicity
+        0b0011_1111, // Flags (URG, ACK, PSH, RST, SYN, FIN)
+        0x00, 0x00, // Window size (just an example)
+    ];
 
-    let mut output = io::stdout();
-    output.write(&buf).unwrap();
+    let tcp_header = TcpHeader::new(&header_bytes);
+    println!("{:?}", tcp_header);
 
-    println!("{}", String::from_utf8_lossy(&buf));
+    // let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
+    // eprintln!("Bound to port {}", socket.local_addr().unwrap().port());
+
+    // let mut buf: [u8; 1500] = [0; 1500];
+    // socket.recv(&mut buf).unwrap();   
+
+    // let mut output = io::stdout();
+    // output.write(&buf).unwrap();
+
+    // println!("{}", String::from_utf8_lossy(&buf));
 }
