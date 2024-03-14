e145ba1a2f9deb500b0b3d547847e85bfb0904cc
jazzcort <jason101011113@gmail.com>
2024-03-14 16:05:53 -0400

    new version

--- sender/src/tcp_sender.rs
+++ sender/src/tcp_sender.rs
@@ -447,7 +447,7 @@ impl Sender {
                 );
 
                 if is_first {
-                    self.ssthresh = self.cwnd * 2 / 3;
+                    self.ssthresh = self.cwnd / 2;
                     is_first = false;
                 }
 
